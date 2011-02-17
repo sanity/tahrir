58d59c64efd2b186024f88d1fcc08f1b5f18fea0
Ian Clarke <ian.clarke@gmail.com>
2011-02-16 18:46:46 -0600

    simple serialization works

--- src/main/java/tahrir/io/serialization/TahrirSerializer.java
+++ src/main/java/tahrir/io/serialization/TahrirSerializer.java
@@ -4,23 +4,53 @@ import java.lang.reflect.*;
 import java.nio.ByteBuffer;
 import java.util.Map;
 
-import tahrir.io.serialization.TahrirSerializable.TahrirSerializableException;
+import tahrir.io.serialization.serializers.*;
 
 import com.google.common.collect.Maps;
 
 public abstract class TahrirSerializer {
+	public static void main(final String[] args) throws TahrirSerializableException {
+		final Object testObject = new TestObject();
+
+
+		final ByteBuffer bb = ByteBuffer.allocate(20);
+
+		serializeTo(testObject, bb);
+
+		final TestObject testObject2 = deserializeFrom(TestObject.class, bb);
+
+		System.out.println(testObject2);
+	}
+
+	public static class TestObject {
+		public int intValue = 1;
+	}
+
 	protected final Type type;
 
 	private static Map<Type, TahrirSerializer> serializers;
 
 	static {
 		serializers = Maps.newHashMap();
+		registerSerializer(new IntegerSerializer(), Integer.class, Integer.TYPE);
+		registerSerializer(new BooleanSerializer(), Boolean.class, Boolean.TYPE);
+		registerSerializer(new ByteSerializer(), Byte.class, Byte.TYPE);
+		registerSerializer(new CharSerializer(), Character.class, Character.TYPE);
+		registerSerializer(new DoubleSerializer(), Double.class, Double.TYPE);
+		registerSerializer(new FloatSerializer(), Float.class, Float.TYPE);
+		registerSerializer(new LongSerializer(), Long.class, Long.TYPE);
+		registerSerializer(new ShortSerializer(), Short.class, Short.TYPE);
+		registerSerializer(new StringSerializer(), String.class);
 	}
 
 	private static final Map<Class<?>, Map<Integer, Field>> fieldMap = Maps.newHashMap();
 
-	public static <T> void registerSerializer(final Type type, final TahrirSerializer serializer) {
-		serializers.put(type, serializer);
+	public static <T> void registerSerializer(final TahrirSerializer serializer, final Type... types) {
+		for (final Type type : types) {
+			final TahrirSerializer put = serializers.put(type, serializer);
+			if (put != null)
+				throw new RuntimeException("Tried to register serializer for "+type+" twice");
+		}
 	}
 
 	protected TahrirSerializer(final Type type) {
@@ -31,7 +61,9 @@ public abstract class TahrirSerializer {
 
 		try {
 			final Field[] fields = object.getClass().getFields();
-			writeLong(bb, fields.length);
+			if (fields.length > 127)
+				throw new TahrirSerializableException("Cannot serialize objects with more than 127 fields");
+			bb.put((byte) fields.length);
 			for (final Field field : fields) {
 				bb.putInt(field.getName().hashCode());
 				final TahrirSerializer fieldSerializer = serializers.get(field.getType());
@@ -42,7 +74,7 @@ public abstract class TahrirSerializer {
 				}
 			}
 		} catch (final Exception e) {
-			throw new TahrirSerializable.TahrirSerializableException(e);
+			throw new TahrirSerializableException(e);
 		}
 	}
 
@@ -51,7 +83,8 @@ public abstract class TahrirSerializer {
 			Map<Integer, Field> fMap = fieldMap.get(c);
 			if (fMap == null) {
 				fMap = Maps.newHashMap();
-				for (final Field field : c.getFields()) {
+				final Field[] fields = c.getFields();
+				for (final Field field : fields) {
 					final Field old = fMap.put(field.getName().hashCode(), field);
 					if (old != null) // This is laughably unlikely
 						throw new RuntimeException("Field "+field.getName()+" of "+c.getName()+" has the same hashCode() as field "+old.getName()+", one of them MUST be renamed");
@@ -59,7 +92,7 @@ public abstract class TahrirSerializer {
 				fieldMap.put(c, fMap);
 			}
 			final T returnObject = c.newInstance();
-			final long fieldCount = readLong(bb);
+			final int fieldCount = bb.get();
 			for (int fix = 0; fix < fieldCount; fix++) {
 				final int fieldHash = bb.getInt();
 				final Field field = fMap.get(fieldHash);
@@ -74,26 +107,30 @@ public abstract class TahrirSerializer {
 		}
 	}
 
-	public static void writeLong(final ByteBuffer bb, long value) {
-		while (value < 0 || value > 127) {
-			bb.put((byte) (0x80 | (value & 0x7F)));
-			value = value >>> 7;
-		}
-		bb.put((byte) value);
-	}
-
-	public static long readLong(final ByteBuffer bb) throws TahrirSerializableException {
-		int shift = 0;
-		int b;
-		long value = 0;
-		while ((b = bb.getInt()) >= 0) {
-			value = value + (b & 0x7f) << shift;
-			shift += 7;
-			if ((b & 0x80) != 0)
-				return value;
-		}
-		throw new TahrirSerializableException("Malformed stop-bit encoding");
-	}
+	// public static void writeLong(final ByteBuffer bb, long value) {
+	// while (value < 0 || value > 127) {
+	// bb.put((byte) (0x80 | (value & 0x7F)));
+	// value = value >>> 7;
+	// }
+	// bb.put((byte) value);
+	// }
+	//
+	// public static long readLong(final ByteBuffer bb) throws
+	// TahrirSerializableException {
+	// int shift = 0;
+	// long value = 0;
+	// while (true) {
+	// final int b = bb.get();
+	// if (b < 0) {
+	// break;
+	// }
+	// value = value + (b & 0x7f) << shift;
+	// shift += 7;
+	// if ((b & 0x80) != 0)
+	// return value;
+	// }
+	// throw new TahrirSerializableException("Malformed stop-bit encoding");
+	// }
 
 	protected abstract Object deserialize(Type type, ByteBuffer bb);
 
