e01ca75403be7bdfb334847b8a13a3633d45cd7b
Kieran Donegan <kdonegan.92@gmail.com>
2012-05-22 11:26:59 +0100

    it's probably better here

---
+++ src/main/java/tahrir/io/net/RemoteNodeAddress.java
@@ -0,0 +1,54 @@
+package tahrir.io.net;
+
+import java.security.interfaces.RSAPublicKey;
+
+
+public class RemoteNodeAddress {
+	public PhysicalNetworkLocation location;
+	public RSAPublicKey publicKey;
+
+	public RemoteNodeAddress() {
+
+	}
+
+	public RemoteNodeAddress(final PhysicalNetworkLocation location,final RSAPublicKey publicKey) {
+		this.location = location;
+		this.publicKey = publicKey;
+	}
+
+
+
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((location == null) ? 0 : location.hashCode());
+		return result;
+	}
+
+	@Override
+	public boolean equals(final Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (!(obj instanceof RemoteNodeAddress))
+			return false;
+		final RemoteNodeAddress other = (RemoteNodeAddress) obj;
+		if (location == null) {
+			if (other.location != null)
+				return false;
+		} else if (!location.equals(other.location))
+			return false;
+		return true;
+	}
+
+	@Override
+	public String toString() {
+		final StringBuilder builder = new StringBuilder();
+		builder.append("RemoteNodeAddress [address=");
+		builder.append(location);
+		builder.append("]");
+		return builder.toString();
+	}
+}
