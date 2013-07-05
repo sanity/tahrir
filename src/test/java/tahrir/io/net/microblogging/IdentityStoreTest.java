f65c3a326545f3e842af872dd7266f8fbb59af11
Ravi Tejasvi <ravitejasvi@gmail.com>
2013-07-05 22:21:52 +0530

    duplicate test fixed.

--- src/test/java/tahrir/io/net/microblogging/IdentityStoreTest.java
+++ src/test/java/tahrir/io/net/microblogging/IdentityStoreTest.java
@@ -15,85 +15,153 @@ import java.util.Set;
  */
 public class IdentityStoreTest {
 
-    File identityStoreTestFile=new File(TrConstants.identityStoreTestFilePath);
-    RSAPublicKey user1Key = TrCrypto.createRsaKeyPair().a;
-    final String user1nick = "name1";
 
-    RSAPublicKey user2Key = TrCrypto.createRsaKeyPair().a;
-    final String user2nick = "name2";
+
+
 
     final String boundingNick = "na";
 
-    IdentityStore testStore=new IdentityStore(identityStoreTestFile);
-    UserIdentity identityOne=new UserIdentity(user1nick, user1Key);
-    UserIdentity identityTwo=new UserIdentity(user2nick, user2Key);
-    final String label= "Following";
 
     @Test
     public void addLabelToIdentityTest(){
+        File identityStoreTestFile=new File(TrConstants.identityStoreTestFilePath);
+        RSAPublicKey user1Key = TrCrypto.createRsaKeyPair().a;
+        final String user1nick = "name1";
+        IdentityStore testStore=new IdentityStore(identityStoreTestFile);
+        UserIdentity identityOne=new UserIdentity(user1nick, user1Key);
+        final String label= "Following";
+
         testStore.addIdentity(label, identityOne);
         Assert.assertTrue(testStore.getIdentitiesWithLabel(label).contains(identityOne));
         Assert.assertTrue(testStore.getLabelsForIdentity(identityOne).contains(label));
+        identityStoreTestFile.delete();
     }
 
     @Test
     public void addIdentityToNickTest(){
+        File identityStoreTestFile=new File(TrConstants.identityStoreTestFilePath);
+        IdentityStore testStore=new IdentityStore(identityStoreTestFile);
+        RSAPublicKey user2Key = TrCrypto.createRsaKeyPair().a;
+        final String user2nick = "name2";
+        UserIdentity identityTwo=new UserIdentity(user2nick, user2Key);
+
+
+
         testStore.addIdentityToUsersWithNickname(identityTwo);
         Assert.assertTrue(testStore.getIdentitiesWithNick(identityTwo.getNick()).contains(identityTwo));
+
+        identityStoreTestFile.delete();
     }
 
     @Test
     public void removeLabelFromIdentityTest(){
+
+        File identityStoreTestFile=new File(TrConstants.identityStoreTestFilePath);
+        RSAPublicKey user1Key = TrCrypto.createRsaKeyPair().a;
+        final String user1nick = "name1";
+        IdentityStore testStore=new IdentityStore(identityStoreTestFile);
+        UserIdentity identityOne=new UserIdentity(user1nick, user1Key);
+        final String label= "Following";
+
+        testStore.addIdentity(label, identityOne);
+
         testStore.removeLabelFromIdentity(label, identityOne);
         Assert.assertFalse(testStore.getIdentitiesWithLabel(label).contains(identityOne));
         Assert.assertFalse(testStore.getLabelsForIdentity(identityOne).contains(label));
+        identityStoreTestFile.delete();
     }
 
     @Test
     public void getIdentitiesWithLabelTest(){
+        File identityStoreTestFile=new File(TrConstants.identityStoreTestFilePath);
+        RSAPublicKey user1Key = TrCrypto.createRsaKeyPair().a;
+        final String user1nick = "name2";
+        IdentityStore testStore=new IdentityStore(identityStoreTestFile);
+        UserIdentity identityTwo=new UserIdentity(user1nick, user1Key);
+        final String label= "Following";
+
         testStore.addIdentity(label, identityTwo);
         Assert.assertTrue(testStore.getIdentitiesWithLabel(label).contains(identityTwo));
+        identityStoreTestFile.delete();
     }
 
     @Test
     public void removeIdentityFromNickTest(){
+        File identityStoreTestFile=new File(TrConstants.identityStoreTestFilePath);
+        IdentityStore testStore=new IdentityStore(identityStoreTestFile);
+        RSAPublicKey user2Key = TrCrypto.createRsaKeyPair().a;
+        final String user2nick = "name2";
+        UserIdentity identityTwo=new UserIdentity(user2nick, user2Key);
+
+        testStore.addIdentityToUsersWithNickname(identityTwo);
         testStore.removeIdentityFromNick(identityTwo);
         Assert.assertFalse(testStore.getIdentitiesWithNick(identityTwo.getNick()).contains(identityTwo));
+        identityStoreTestFile.delete();
     }
 
     @Test
     public void getLabelsForIdentityTest(){
+        File identityStoreTestFile=new File(TrConstants.identityStoreTestFilePath);
+        RSAPublicKey user1Key = TrCrypto.createRsaKeyPair().a;
+        final String user1nick = "name2";
+        IdentityStore testStore=new IdentityStore(identityStoreTestFile);
+        UserIdentity identityTwo=new UserIdentity(user1nick, user1Key);
+        final String label= "Following";
+
+        testStore.addIdentity(label, identityTwo);
         Assert.assertTrue(testStore.getLabelsForIdentity(identityTwo).contains(label));
+        identityStoreTestFile.delete();
     }
 
     @Test
     public void getUserIdentitiesStartingWithTest(){
+        File identityStoreTestFile=new File(TrConstants.identityStoreTestFilePath);
+        RSAPublicKey user1Key = TrCrypto.createRsaKeyPair().a;
+        final String user1nick = "name1";
+        IdentityStore testStore=new IdentityStore(identityStoreTestFile);
+        UserIdentity identityOne=new UserIdentity(user1nick, user1Key);
+
         testStore.addIdentityToUsersWithNickname(identityOne);
         Assert.assertTrue(testStore.getUserIdentitiesStartingWith(boundingNick).contains(identityOne));
+        identityStoreTestFile.delete();
     }
 
     @Test
     public void getIdentitiesWithNick(){
+        File identityStoreTestFile=new File(TrConstants.identityStoreTestFilePath);
+        RSAPublicKey user1Key = TrCrypto.createRsaKeyPair().a;
+        final String user1nick = "name1";
+        IdentityStore testStore=new IdentityStore(identityStoreTestFile);
+        UserIdentity identityOne=new UserIdentity(user1nick, user1Key);
+
         testStore.addIdentityToUsersWithNickname(identityOne);
         Assert.assertTrue(testStore.getIdentitiesWithNick(identityOne.getNick()).contains(identityOne));
+        identityStoreTestFile.delete();
     }
     @Test
     public void duplicateUsersTest(){
+        File identityStoreTestFile=new File(TrConstants.identityStoreTestFilePath);
+        IdentityStore testStore=new IdentityStore(identityStoreTestFile);
+
         UserIdentity testUser2 = new UserIdentity("TestUser2", TrCrypto.createRsaKeyPair().a);
+
         testStore.addIdentity("Friends", testUser2);
         testStore.addIdentity("Friends", testUser2);
+
         Assert.assertFalse((testStore.getIdentitiesWithNick(testUser2.getNick()).size())>1);
+        identityStoreTestFile.delete();
     }
 
    @Test
     public void fileLoadingTest(){
-        testStore.addIdentity("Following", identityTwo);
-        UserIdentity testUser2 = new UserIdentity("TestUser2", TrCrypto.createRsaKeyPair().a);
-        testStore.addIdentity("Friends", testUser2);
-       UserIdentity testUser3 = new UserIdentity("TestUser2", TrCrypto.createRsaKeyPair().a);
+       File identityStoreTestFile=new File(TrConstants.identityStoreTestFilePath);
+       IdentityStore testStore=new IdentityStore(identityStoreTestFile);
+
+       UserIdentity testUser3 = new UserIdentity("TestUser3", TrCrypto.createRsaKeyPair().a);
        testStore.addIdentity("Friends", testUser3);
-        IdentityStore testStore2=new IdentityStore(identityStoreTestFile);
-        Assert.assertTrue(testStore2.hasIdentityInIdStore(testUser2));
+       IdentityStore testStore2=new IdentityStore(identityStoreTestFile);
+       Assert.assertTrue(testStore2.hasIdentityInIdStore(testUser3));
+       identityStoreTestFile.delete();
     }
 
 }
