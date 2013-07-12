e442ed7402a023628852c0362e4d76546ba843b6
Ravi Tejasvi <ravitejasvi@gmail.com>
2013-07-12 22:02:18 +0530

    Adding missing files

---
+++ src/main/java/tahrir/ui/ContactBookDisplayPage.java
@@ -0,0 +1,116 @@
+package tahrir.ui;
+
+import com.google.common.collect.Maps;
+import com.google.common.eventbus.EventBus;
+import com.google.common.eventbus.Subscribe;
+import tahrir.io.net.microblogging.UserIdentity;
+import tahrir.io.net.microblogging.microblogs.ParsedMicroblog;
+
+import javax.swing.*;
+import javax.swing.table.AbstractTableModel;
+import java.awt.*;
+import java.util.*;
+
+/**
+ * Author   : Ravisvi <ravitejasvi@gmail.com>
+ * Date     : 8/7/13
+ */
+public class ContactBookDisplayPage {
+    private final JComponent content;
+    private final ContactBookTableModel tableModel;
+    EventBus eventBus;
+
+    public ContactBookDisplayPage(final TrMainWindow mainWindow) {
+        eventBus = mainWindow.node.eventBus;
+        tableModel = new ContactBookTableModel();
+        for (Map.Entry<UserIdentity, Set<String>> pairs : mainWindow.node.identityStore.labelsOfUser.entrySet()){
+            {
+                tableModel.addNewIdentity(pairs.getKey());
+            }
+        }
+
+        final JTable table = new JTable(tableModel);
+        table.setFillsViewportHeight(true);
+        // TODO: change the size as needed
+        table.setRowHeight(50);
+
+        table.setFont(new Font("Arial", Font.BOLD, 12));
+        table.setGridColor(new Color(244,242,242));
+        final JScrollPane scrollPane = new JScrollPane();
+        scrollPane.setHorizontalScrollBarPolicy(JScrollPane.HORIZONTAL_SCROLLBAR_NEVER);
+        scrollPane.setVerticalScrollBarPolicy(JScrollPane.VERTICAL_SCROLLBAR_AS_NEEDED);
+        scrollPane.setViewportView(table);
+        content = scrollPane;
+
+        eventBus.register(this);
+
+    }
+
+    @Subscribe
+    public void identityModified(IdentityModifiedEvent event){
+        if(event.type.equals(IdentityModifiedEvent.IdentityModificationType.ADD)){
+                tableModel.addNewIdentity(event.identity);
+        }
+        else{
+            tableModel.removeIdentity(event.identity);
+        }
+    }
+
+    public JComponent getContent() {
+        return content;
+    }
+
+    private class ContactBookTableModel extends AbstractTableModel{
+        private final ArrayList<UserIdentity> users;
+
+        public ContactBookTableModel() {
+            users = new ArrayList<UserIdentity>();
+        }
+
+        @Override
+        public int getColumnCount() {
+            return 1;
+        }
+
+        @Override
+        public int getRowCount() {
+            return users.size();
+        }
+
+        @Override
+        public Object getValueAt(final int row, final int col) {
+            return ("        "+users.get(row).getNick());
+            //Indenting it.
+        }
+
+        public void addNewIdentity(UserIdentity identity) {
+            if(!(users.contains(identity))){
+            users.add(identity);
+            fireTableDataChanged();
+            }
+        }
+
+        public void removeIdentity(UserIdentity identity) {
+            final int identityIndex = users.indexOf(identity);
+            users.remove(identityIndex);
+        }
+
+        @Override
+        public Class<?> getColumnClass(final int columnIndex) {
+            return UserIdentity.class;
+        }
+
+        @Override
+        public String getColumnName(final int columnIndex) {
+            return null;
+        }
+
+        @Override
+        public boolean isCellEditable(final int rowIndex, final int columnIndex) {
+            // this allows clicking of buttons etc. in the table
+            return true;
+        }
+    }
+
+
+}
---
+++ src/main/java/tahrir/ui/IdentityModifiedEvent.java
@@ -0,0 +1,20 @@
+package tahrir.ui;
+
+
+import tahrir.io.net.microblogging.UserIdentity;
+
+/**
+ * Author   : Ravisvi <ravitejasvi@gmail.com>
+ * Date     : 11/7/13
+ */
+public class IdentityModifiedEvent {
+    UserIdentity identity;
+    public enum IdentityModificationType {
+        ADD, MODIFY, DELETE
+    }
+    IdentityModificationType type;
+    public IdentityModifiedEvent(UserIdentity identity, IdentityModificationType type){
+        this.identity = identity;
+         this.type = type;
+    }
+}
