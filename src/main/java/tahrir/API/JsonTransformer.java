4238567a4f51fbef7ee786f45e555c3f0bd78281
Dharamsitejas <tejas.dharamsi@gmail.com>
2014-06-11 21:47:16 +0530

    Json Spark

---
+++ src/main/java/tahrir/API/JsonTransformer.java
@@ -0,0 +1,26 @@
+/**
+ * Created by Tejas Dharamsi on 6/11/2014.
+ */
+import static spark.Spark.*;
+import spark.*;
+
+import spark.ResponseTransformerRoute;
+
+public abstract class JsonTransformer extends ResponseTransformerRoute {
+
+    private Gson gson = new Gson();
+
+    protected JsonTransformer(String path) {
+        super(path);
+    }
+
+    protected JsonTransformer(String path, String acceptType) {
+        super(path, acceptType);
+    }
+
+    @Override
+    public String render(Model model) {
+        return gson.toJson(model.getModel());
+    }
+
+}
