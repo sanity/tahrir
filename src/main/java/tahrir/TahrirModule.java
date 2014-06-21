5699c457ec133b4f827cf6144ee54349a93a7bf8
Ian Clarke <ian.clarke@gmail.com>
2014-06-21 17:12:02 -0500

    start a module

---
+++ src/main/java/tahrir/TahrirModule.java
@@ -0,0 +1,12 @@
+package tahrir;
+
+import dagger.Module;
+import tahrir.io.net.TrSessionManager;
+
+/**
+ * Created by ian on 6/21/14.
+ */
+@Module(injects = {TrSessionManager.class})
+public class TahrirModule {
+
+}
