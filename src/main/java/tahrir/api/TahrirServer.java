32884f838346624642ba022b7fcbba68618aa93f
Dharamsitejas <tejas.dharamsi@gmail.com>
2014-07-04 19:20:51 +0530

    Trying get method with json

--- src/main/java/tahrir/api/TahrirServer.java
+++ src/main/java/tahrir/api/TahrirServer.java
@@ -3,6 +3,7 @@ package tahrir.api;
 /**
  * Created by Tejas Dharamsi on 6/25/2014.
  */
+import org.restlet.Component;
 import org.restlet.Server;
 import org.restlet.data.Protocol;
 
@@ -11,5 +12,13 @@ public class TahrirServer {
 
         Server server = new Server(Protocol.HTTP,8080,TahrirServerResource.class);
         server.start();
+
+        Component c1 = new Component();
+       c1.getServers().add(Protocol.HTTP, 8080);
+
+        GetMessage restlet = new GetMessage();
+
+        c1.getDefaultHost().attach("/fetch_message", restlet);
+        c1.start();
     }
 }
