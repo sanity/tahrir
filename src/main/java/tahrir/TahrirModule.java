10f5d1f6b9975a7098f64b171c20a09faa08d65d
Ravi Tejasvi <ravitejasvi@gmail.com>
2014-07-10 17:44:06 +0530

    Tahrir module
    [#71985360]

---
+++ src/main/java/tahrir/TahrirModule.java
@@ -0,0 +1,24 @@
+package tahrir;
+
+import dagger.Module;
+import dagger.Provides;
+import tahrir.ui.swingUI.GUIMain;
+
+
+
+/**
+ * User: ravisvi <ravitejasvi@gmail.com>
+ * Date: 10/07/14
+ */
+
+
+@Module(
+        injects = GUIMain.class,
+        complete = true
+)
+
+public class TahrirModule {
+
+
+
+}
