825697519b6b29d45d754ae87f9d21dd6c2777b4
Ravi Tejasvi <ravitejasvi@gmail.com>
2014-07-27 19:34:28 +0530

    Trying to fix Tuple2 of public key private key
    [#71985360]

--- src/main/java/tahrir/TahrirModule.java
+++ src/main/java/tahrir/TahrirModule.java
@@ -1,16 +1,26 @@
 package tahrir;
 
+import com.google.common.base.Function;
 import dagger.Module;
 import dagger.Provides;
 import tahrir.network.RemoteNodeAddress;
 import tahrir.transport.messaging.udpV1.PhysicalNetworkLocation;
+import tahrir.transport.messaging.udpV1.UdpNetworkInterface;
+import tahrir.transport.rpc.TrNetworkInterface;
 import tahrir.transport.rpc.TrPeerManager;
+import tahrir.transport.rpc.TrRemoteConnection;
+import tahrir.transport.rpc.TrSessionManager;
 import tahrir.ui.swingUI.GUIMain;
+import tahrir.util.tools.ByteArraySegment;
 import tahrir.util.tools.Persistence;
+import tahrir.util.tools.Tuple2;
 
 import javax.inject.Named;
 import java.io.File;
 import java.io.IOException;
+import java.net.SocketException;
+import java.security.interfaces.RSAPrivateKey;
+import java.security.interfaces.RSAPublicKey;
 
 import static tahrir.util.tools.TrUtils.TestUtils.createTempDirectory;
 
@@ -28,6 +38,7 @@ import static tahrir.util.tools.TrUtils.TestUtils.createTempDirectory;
 
 public class TahrirModule {
     private File rootDirectory = null;
+    UdpNetworkInterface.UNIConfig config = null;
 
 
     @Provides
@@ -38,6 +49,27 @@ public class TahrirModule {
     @Provides
     TrPeerManager.Config provideConfig(){
         return new TrPeerManager.Config();
+    }
+
+    @Provides @Named("keys")
+    Tuple2<RSAPublicKey, RSAPrivateKey> providesTuple(){
+
+    }
+
+    @Provides
+    TrNetworkInterface provideNetworkInterface(UdpNetworkInterface.UNIConfig config, Tuple2<RSAPublicKey, RSAPrivateKey> keyPair) throws SocketException {
+        return new UdpNetworkInterface(config, keyPair);
+    }
+
+    @Provides @Named("AllowUnilateral")
+    TrSessionManager provideTrSessionManagerUnilateral(TrNetworkInterface trNetworkInterface){
+        return new TrSessionManager(trNetworkInterface, true);
+    }
+
+
+    @Provides @Named("DisallowUnilateral")
+    TrSessionManager provideTrSessionManager(TrNetworkInterface trNetworkInterface){
+        return new TrSessionManager(trNetworkInterface, false);
     }
     /*
     @Provides   //for rootDirectory file.
@@ -50,6 +82,14 @@ public class TahrirModule {
        return new File(directory, filename);
     }
     */
+
+    @Provides
+    UdpNetworkInterface.UNIConfig provideUNIConfig(){
+        if(this.config==null){
+            this.config = new UdpNetworkInterface.UNIConfig();
+        }
+        return this.config;
+    }
 
     //TODO: This may give a problem is provideRootDir isn't called before other functions. Should it be lazy?
 
@@ -70,7 +110,7 @@ public class TahrirModule {
             if (!(this.rootDirectory.mkdir()))
                 throw new IOException("Could not create temp directory: " + this.rootDirectory.getAbsolutePath());
             } catch (IOException e) {
-                e.printStackTrace();  //To change body of catch statement use File | Settings | File Templates.
+                e.printStackTrace();
             }
             return (this.rootDirectory);
         }
